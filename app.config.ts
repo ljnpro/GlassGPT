242d4d8082bf3b4be66db90c3aa514a6f78a55b3
Jianuo Li <187979738+ljnpro@users.noreply.github.com>
2026-03-14 16:16:51 -0400

    fix: render and save generated images

--- app.config.ts
+++ app.config.ts
@@ -31,7 +31,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "2.1.23",
+  version: "2.1.24",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
